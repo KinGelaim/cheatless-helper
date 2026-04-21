8a388310087c29d1ce323e7fddfe4f4c31579edc
KinGelaim <89123232156@mail.ru>
2026-04-21 22:33:17 +0500

    Deleted an unused type

--- OverlootingAutoClick/OverlootingAutoClick/Resources/ResourceInfo.cs
+++ OverlootingAutoClick/OverlootingAutoClick/Resources/ResourceInfo.cs
@@ -3,11 +3,9 @@ using Emgu.CV;
 namespace OverlootingAutoClick.Resources;
 
 internal sealed class ResourceInfo(
-    ResourceType type,
     string name,
     string path) : IDisposable
 {
-    public ResourceType Type { get; init; } = type;
     public string Name { get; init; } = name;
     public string Path { get; init; } = path;
     public Mat? ImageMat { get; set; }
